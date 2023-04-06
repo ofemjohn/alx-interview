e8b3c00e0cebbe271277ced0af7cd1ddf2f279cd
ofemjohn <ofemjohn@gmail.com>
2023-04-07 00:32:32 +0100

    fixed pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,17 +9,13 @@ arguments value
 def minOperations(n):
     if n < 1:
         return 0
-    
     clipboard_copy = 1
     current_h = 1
     num_ops_count = 0
-    
     while current_h < n:
         if n % current_h == 0:
             clipboard_copy = current_h
             num_ops_count += 1
-        
         current_h += clipboard_copy
         num_ops_count += 1
-        
     return num_ops_count
