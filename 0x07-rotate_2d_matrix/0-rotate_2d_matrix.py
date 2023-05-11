bbd2eb19ee24ce78888540e2d33814de383a2257
ofemjohn <ofemjohn@gmail.com>
2023-05-11 21:40:32 +0100

    rotate a matrix by 90 degrees

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+'''rotate matrix '''
+
 
 def rotate_2d_matrix(matrix):
-    '''rotates a 2D matrix.'''
     m = len(matrix)
 
-    # Transpose the matrix
+    '''Transpose the matrix'''
     for i in range(m):
         for j in range(i, m):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # Reverse each row
+    ''' Reverse each row of the transposed matrix'''
     for i in range(m):
-        matrix[i] = matrix[i][::-1]
+        for j in range(m // 2):
+            matrix[i][j], matrix[i][m - j -
+                                    1] = matrix[i][m - j - 1], matrix[i][j]
