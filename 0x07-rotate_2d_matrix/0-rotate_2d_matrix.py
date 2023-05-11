a81b3398e3af2d06c23292c2c331d357bd523e19
ofemjohn <ofemjohn@gmail.com>
2023-05-11 21:31:04 +0100

    rotate a matrix by 90 degrees

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,30 +1,12 @@
-#!/usr/bin/env python
 def rotate_2d_matrix(matrix):
-    """
-    Given an n x n 2D matrix, rotate it 90 degrees clockwise. The matrix must be edited in-place.
-    
-    Parameters:
-    matrix (list): The 2D matrix to be rotated
-    
-    Returns:
-    None
-    """
-    try:
-        # Check if matrix is not empty
-        if not matrix:
-            raise ValueError("Matrix cannot be empty")
-        
-        # Get the length of the matrix
-        n = len(matrix)
-        
-        # Transpose the matrix
-        for i in range(n):
-            for j in range(i, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
-        # Reverse each row of the matrix
-        for i in range(n):
-            matrix[i] = matrix[i][::-1]
-    except ValueError as e:
-        # Log the error
-        print(f"Error: {e}")
+    '''rotates a 2D matrix.'''
+    m = len(matrix)
+
+    # Transpose the matrix
+    for i in range(m):
+        for j in range(i, m):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(m):
+        matrix[i] = matrix[i][::-1]
--- 0x07-rotate_2d_matrix/teste.py
+++ 0x07-rotate_2d_matrix/teste.py
@@ -1,13 +1,20 @@
-def rotate_2d_matrix(matrix):
-    n = len(matrix)
-    # Transpose matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    # Reverse each row
-    for i in range(n):
-        matrix[i] = matrix[i][::-1]
+#!/usr/bin/python3
 
+""" Rotate 2D Matrix 90 Degrees Clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Function for rotating 2D Matrix 90 degrees clockwise
+    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+    - Do not return anything. The matrix must be edited in-place.
+    - You can assume the matrix will have 2 dimensions and will not be empty.
+    """
+
+    n = len(matrix[0])
+
+    for i in range(n - 1, -1, -1):
+        for j in range(0, n):
+            matrix[j].append(matrix[i].pop(0))
 
 matrix = [[1, 2, 3],
          [4, 5, 6],
