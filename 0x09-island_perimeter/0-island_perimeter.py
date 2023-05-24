2a8a36007c1f06122ec43ceb0281a19e040acd4e
ofemjohn <ofemjohn@gmail.com>
2023-05-24 10:22:49 +0100

    added shebang and checked for pycodestyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,20 +1,22 @@
+#!/usr/bin/python3
+
 def island_perimeter(grid):
-  '''
-  Args:
-    grid: A list of list of integers.
+    '''
+    Args:
+      grid: A list of list of integers.
 
-  Returns:
-    The perimeter of the island.
-  '''
+    Returns:
+      The perimeter of the island.
+    '''
 
-  island_perimeter = 0
-  for i in range(len(grid)):
-    for j in range(len(grid[0])):
-      if grid[i][j] == 1:
-        island_perimeter += 4
-        if i > 0 and grid[i - 1][j] == 1:
-          island_perimeter -= 2
-        if j > 0 and grid[i][j - 1] == 1:
-          island_perimeter -= 2
+    island_perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                island_perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    island_perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    island_perimeter -= 2
 
-  return island_perimeter
+    return island_perimeter
